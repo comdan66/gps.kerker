7248c637e551f555e3ce128ca28d0301e079ba15
OA Wu <comdan66@gmail.com>
2018-05-29 13:40:09 +0800

    Fix code

--- app/controller/api/Locations.php
+++ app/controller/api/Locations.php
@@ -66,7 +66,7 @@ class Locations extends ApiController {
       $ids || $ids = array ();
       foreach ($posts['points'] as $point)
         if ($obj = Location::create ($point))
-          array_push ($ids, $obj->id);
+          array_push ($ids, (int)$obj->id);
 
       return true;
     };

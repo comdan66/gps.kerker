717453bc8bb0030e7f913cd025693ea417233238
OA Wu <comdan66@gmail.com>
2018-06-03 15:35:10 +0800

    Fix code

--- assets/js/site/Main/index.js
+++ assets/js/site/Main/index.js
@@ -5,12 +5,13 @@
  * @link        https://www.ioa.tw/
  */
 
-// Array.prototype.max = function (k) {
-//   return Math.max.apply (null, this.column (k));
-// };
 window.gmc = function () { $(window).trigger ('gm'); };
+
 function OAGM(t){this._div=null,this._option=Object.assign({className:"",top:0,left:0,width:32,height:32,html:"",map:null,position:null,css:{}},t),this._option.map&&this.setMap(this._option.map)}function initOAGM(){OAGM.prototype=new google.maps.OverlayView,Object.assign(OAGM.prototype,{setPoint:function(){if(!this._option.position)return this._div.style.left="-999px",void(this._div.style.top="-999px");var t=this.getProjection().fromLatLngToDivPixel(this._option.position);t&&(this._div.style.left=t.x-this._option.width/2+this._option.left+"px",this._div.style.top=t.y-this._option.height/2+this._option.top+"px")},draw:function(){if(!this._div){for(var t in this._div=document.createElement("div"),this._div.style.position="absolute",this._div.className=this._option.className,this._div.style.width=this._option.width+"px",this._div.style.height=this._option.height+"px",this._div.innerHTML=this._option.html,this._option.css)"width"!=t&&"height"!=t&&"top"!=t&&"left"!=t&&"bottom"!=t&&"right"!=t&&(this._div.style[t]=this._option.css[t]);var i=this;google.maps.event.addDomListener(this._div,"click",function(t){t.stopPropagation&&t.stopPropagation(),google.maps.event.trigger(i,"click")}),this.getPanes().overlayImage.appendChild(this._div)}this.setPoint()},remove:function(){return this._div&&(this._div.parentNode.removeChild(this._div),this._div=null),this},setWidth:function(t){return this._div&&(this._option.width=t,this._div.style.width=this._option.width+"px",this.setPoint()),this},setHeight:function(t){return this._div&&(this._option.height=t,this._div.style.height=this._option.height+"px",this.setPoint()),this},setTop:function(t){return this._div&&(this._option.top=t,this._div.style.top=this._option.top+"px",this.setPoint()),this},setLeft:function(t){return this._div&&(this._option.left=t,this._div.style.left=this._option.left+"px",this.setPoint()),this},setHtml:function(t){return this._div&&(this._option.html=t,this._div.innerHTML=this._option.html),this},setCss:function(t){if(!this._div)return this;for(var i in this._option.css=t,this._option.css)"width"!=i&&"height"!=i&&"top"!=i&&"left"!=i&&"bottom"!=i&&"right"!=i&&(this._div.style[i]=this._option.css[i]);return this},setClassName:function(t){return this._div&&(this._option.className=t,this._div.className=this._option.className),this},getClassName:function(){return this._option.className},setPosition:function(t){return this.map&&(this._option.position=t,this.setPoint()),this},getPosition:function(){return this._option.position}})}
+var OAMC=function(t){this.uses=[],this.tmp=[],this.opts=Object.assign({map:null,unit:3,useLine:!1,middle:!0,latKey:"a",lngKey:"n",varKey:null,markersKey:null},t)};Object.assign(OAMC.prototype,{clean:function(){this.uses=[],this.tmp=[]},markers:function(n){if(!this.opts.map)return[];var a=this,t=this.opts.map.zoom,e=0,s=0,o=n.length;for(a.clean();e<o;e++)if(!a.uses[e])for(a.tmp[e]={m:[n[e]],a:n[e][a.opts.latKey],n:n[e][a.opts.lngKey]},a.uses[e]=!0,s=e+1;s<o;s++)if(!a.uses[s])if(30/Math.pow(2,t)/a.opts.unit<=Math.max(Math.abs(n[e][a.opts.latKey]-n[s][a.opts.latKey]),Math.abs(n[e][a.opts.lngKey]-n[s][a.opts.lngKey]))){if(a.opts.useLine)break}else a.uses[s]=!0,a.tmp[e].m.push(n[s]);var p=[];return a.tmp.forEach(function(t,e){var s=a.opts.middle?new google.maps.LatLng(t.m.map(function(t){return t[a.opts.latKey]}).reduce(function(t,e){return t+e})/t.m.length,t.m.map(function(t){return t[a.opts.lngKey]}).reduce(function(t,e){return t+e})/t.m.length):new google.maps.LatLng(t.a,t.n);null!==a.opts.markersKey&&(s[a.opts.markersKey]=t),null!==a.opts.varKey&&(s[a.opts.varKey]=n[e]),p.push(s)}),a.clean(),p}});
+
 function ful (l) { return (l / 1000).toFixed (2); }
+
 $(function () {
   var $body = $('body');
   var _gmap = null;
@@ -23,7 +24,6 @@ $(function () {
   var $_length = null;
   var $_duration = null;
   var _cs = ['#CCDDFF', '#99BBFF', '#5599FF', '#0066FF', '#0044BB', '#003C9D', '#003377', '#550088', '#770077'];
-
 
   window.oaGmap = {
     keys: ['AIzaSyApe66UP8VJNwSVufAi0rr9dpq7ON0Dq6Y'],
@@ -71,147 +71,31 @@ $(function () {
     }));
   }
 
-  var Clustering = function (opts) {
-    // this.lats = [];
-    // this.lngs = [];
-    // this.merges = [];
+  function rePath (f) {
+    path (new OAMC ({map: _gmap, unit: 2, useLine: true, varKey: '_v'}).markers (_ps));
+    
+    _ms = _ms.map (function (t) { t instanceof OAGM && t.setMap (null); t = null; return null; }).filter (function (t) { return t; });
+    _ms = new OAMC ({map: _gmap, unit: 0.8, useLine: true, varKey: '_v'}).markers (_ps).map (function (t) {
+      var s = parseInt ((_cs.length / _max) * t._v.s, 10);
 
-    this.uses = [];
-    this.tmp = [];
-
-    this.opts = Object.assign ({
-      map: null,
-      unit: 3,
-      useLine: false,
-      middle: true,
-
-      latKey: 'a',
-      lngKey: 'n',
-      varKey: null,
-      markersKey: null,
-
-    }, opts);
-  };
-
-  Object.assign (
-    Clustering.prototype, {
-      clean: function () {
-        this.uses = [];
-        this.tmp = [];
-      },
-      markers: function (arr) {
-        if (!this.opts.map)
-          return [];
-
-        var that = this,
-            z = this.opts.map.zoom,
-            i = 0,
-            j = 0,
-            c = arr.length;
-
-        that.clean ();
-
-        for (; i < c; i++) {
-          if (that.uses[i])
-            continue;
-
-          that.tmp[i] = {
-            m: [arr[i]],
-            a: arr[i][that.opts.latKey],
-            n: arr[i][that.opts.lngKey],
-          };
-          that.uses[i] = true;
-
-          for (j = i + 1; j < c; j++) {
-            if (that.uses[j])
-              continue;
-
-            if ((30 / Math.pow (2, z)) / that.opts.unit <= Math.max (Math.abs (arr[i][that.opts.latKey] - arr[j][that.opts.latKey]), Math.abs (arr[i][that.opts.lngKey] - arr[j][that.opts.lngKey])))
-              if (that.opts.useLine)
-                break;
-              else
-                continue;
-
-            that.uses[j] = true;
-            that.tmp[i].m.push (arr[j]);
-          }
-        }
-
-
-        var ms = [];
-
-        that.tmp.forEach (function (t, i) {
-
-          var tmp = that.opts.middle ?
-            new google.maps.LatLng (t.m.map (function (u) { return u[that.opts.latKey]; }).reduce (function (a, b) { return a + b; }) / t.m.length, t.m.map (function (u) { return u[that.opts.lngKey]; }).reduce (function (a, b) { return a + b; }) / t.m.length) :
-            new google.maps.LatLng (t.a, t.n);
-
-          if (that.opts.markersKey !== null)
-            tmp[that.opts.markersKey] = t;
-
-          if (that.opts.varKey !== null)
-            tmp[that.opts.varKey] = arr[i];
-
-          ms.push (tmp);
-        });
-
-        that.clean ();
-
-        return ms;
-      }
-    }
-  );
-
-  // 座標陣列, 單位[3], 紀錄陣列, 線性, 合併
-  function calc (v, k, m, l, s) {
-    k = k ? k : 3;
-    var z = _gmap.zoom, u = [], zs = [], i = 0, j = 0, c = v.length;
-
-    for (; i < c; i++) {
-      if (v[i]._h)
-        continue;
-
-      zs[i] = {};
-      zs[i]._m = [v[i]];
-      zs[i]._v = v[i];
-      v[i]._h = true;
-
-      for (j = i + 1; j < c; j++) {
-        if (v[j]._h)
-          continue;
-
-        if ((30 / Math.pow (2, z)) / k <= Math.max (Math.abs (v[i].a - v[j].a), Math.abs (v[i].n - v[j].n)))
-          if (l)
-            break;
-          else
-            continue;
-
-        v[j]._h = true;
-        zs[i]._m.push (v[j]);
-      }
-    }
-
-    for (i = 0; i < c; i++)
-      v[i]._h = false;
-
-    u = [];
-
-    zs.forEach (function (t) {
-      var x = new google.maps.LatLng (t._v.a, t._v.n);
-
-      if (s) {
-        var a = t._m.map (function (u) { return u.a; }).reduce (function (a, b) { return a + b; }) / t._m.length;
-        var n = t._m.map (function (u) { return u.n; }).reduce (function (a, b) { return a + b; }) / t._m.length;
-        
-        x = new google.maps.LatLng (a, n);
-      }
-      if (m) x._m = t._m;
-
-      x._v = t._v;
-      u.push (x);
+      return new OAGM ({
+        map: _gmap,
+        position: t,
+        width: 20,
+        height: 20,
+        className: 'point r' + parseInt (t._v.c, 10),
+        css: {
+          'border': '3px solid ' + (_cs[s] ? _cs[s] : _cs[_cs.length - 1]),
+          'color': _cs[s] ? _cs[s] : _cs[_cs.length - 1]
+        }});
     });
 
-    return u;
+    if (!f || !_ms.length)
+      return;
+
+    var bounds = new google.maps.LatLngBounds ();
+    _ms.forEach (function (t, i) { bounds.extend (t.getPosition ()); });
+    _gmap.fitBounds (bounds);
   }
 
   function ajax (url, first) {
@@ -231,57 +115,13 @@ $(function () {
     }.bind ($(this)));
   }
 
-  function rePath (f) {
-    
-    path (new Clustering ({
-      map: _gmap,
-      unit: 2,
-      middle: true,
-      useLine: true,
-      varKey: '_v'
-    }).markers (_ps));
-    
-    _ms = _ms.map (function (t) { t instanceof OAGM && t.setMap (null); t = null; return null; }).filter (function (t) { return t; });    
-    _ms = new Clustering ({
-      map: _gmap,
-      unit: 0.8,
-      middle: true,
-      useLine: true,
-      varKey: '_v'
-    }).markers (_ps);
-
-    _ms = _ms.map (function (t) {
-      var s = parseInt ((_cs.length / _max) * t._v.s, 10);
-
-      return new OAGM ({
-        map: _gmap,
-        position: t,
-        width: 20,
-        height: 20,
-        className: 'point r' + parseInt (t._v.c, 10),
-        css: {
-          'border': '3px solid ' + (_cs[s] ? _cs[s] : _cs[_cs.length - 1]),
-          'color': _cs[s] ? _cs[s] : _cs[_cs.length - 1]
-        }});
-    });
-
-    if (!f || !_ms.length)
-      return;
-
-
-    var bounds = new google.maps.LatLngBounds ();
-    _ms.forEach (function (t, i) {
-      bounds.extend (t.getPosition ());
-    });
-    _gmap.fitBounds (bounds);
-  }
-
   oaGmap.addFunc (function () {
     initOAGM ();
 
     var $maps = $('#maps');
     var $gmap = $('<div />').addClass ('gmap').appendTo ($maps);
     var $zoom = $('<div />').addClass ('zoom').append ($('<a />').addClass ('icon-02')).append ($('<a />').addClass ('icon-01')).appendTo ($maps);
+    
     $_cs  = $('<div />').addClass ('colors').appendTo ($maps);
     $_length  = $('<div />').addClass ('length').appendTo ($maps);
     $_duration  = $('<div />').addClass ('duration').appendTo ($maps);
@@ -293,14 +133,6 @@ $(function () {
     _gmap.setMapTypeId ('style1');
   
     $zoom.find ('a').click (function () { _gmap.setZoom (_gmap.zoom + ($(this).index () ? -1 : 1)); });
-
-    // var a = new google.maps.Polyline ({
-    //   map: _gmap,
-    //   strokeColor: 'rgba(66, 133, 244, 1.00)',
-    //   strokeWeight: 3,
-    // });
-    // console.error ();
-    
 
     ajax ($maps.data ('url'), true);
     setInterval (ajax.bind (this, $maps.data ('url'), false), 1000 * 10);

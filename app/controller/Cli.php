68b5c9cb8b4a91b276f396524eb8f6a5d160a668
OA Wu <comdan66@gmail.com>
2018-06-04 15:39:41 +0800

    fix code

--- app/controller/Cli.php
+++ app/controller/Cli.php
@@ -19,6 +19,9 @@ class Cli extends Controller {
   }
 
   public function x ($id) {
+    echo '<meta http-equiv="Content-type" content="text/html; charset=utf-8" /><pre>';
+    var_dump ($id);
+    exit ();
     if (!$event = Event::find_by_id ($id))
       exit;
 

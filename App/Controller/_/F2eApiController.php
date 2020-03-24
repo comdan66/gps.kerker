e7e4bfc48299bea393b0fcd194002a595674941f
OA Wu <comdan66@gmail.com>
2020-03-24 18:34:52 +0800

    tmp

--- App/Controller/_/F2eApiController.php
+++ App/Controller/_/F2eApiController.php
@@ -4,13 +4,7 @@ abstract class F2eApiController extends ApiController {
   public function __construct() {
     parent::__construct();
 
-    if (ENVIRONMENT === 'Production')
-      Status::append("Access-Control-Allow-Origin: https://gps.kerker.tw");
-    else if (ENVIRONMENT === 'Testing')
-      Status::append("Access-Control-Allow-Origin: https://testing-gps.kerker.tw");
-    else
-      Status::append("Access-Control-Allow-Origin: http://dev.gps.kerker.tw:8090");
-
+    Status::append("Access-Control-Allow-Origin: " . \config('F2e', 'baseUrl'));
     Status::append("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
     Status::append("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, X-Requested-With, Authorization");
   }
--- App/Model/Event.php
+++ App/Model/Event.php
@@ -23,6 +23,41 @@ class Event extends Model {
   public function genCode() {
     \Load::lib('Code');
     $this->code = \Code::encode($this->id, 5);
+
+    $url = \config('F2e', 'baseUrl') . '/?' . $this->code;
+    self::lineNotify('oa', '要回家囉，關注最新位置請點：' . $url);
+    ENVIRONMENT == 'Development' || self::lineNotify('shari', '要回家囉，關注最新位置請點：' . $url);
+
     return $this->save();
+  }
+  private function lineNotify($tokenKey, $message) {
+    if (!$token = \config('LineNotify', $tokenKey))
+      return false;
+
+    $url = 'https://notify-api.line.me/api/notify';
+    
+    $options = [
+      CURLOPT_URL => $url,
+      CURLOPT_TIMEOUT => 30,
+      CURLOPT_CONNECTTIMEOUT => 30,
+      CURLOPT_RETURNTRANSFER => true,
+      CURLOPT_IPRESOLVE => CURL_IPRESOLVE_V4,
+      CURLOPT_HTTPHEADER => [
+        'Authorization: Bearer ' . $token
+      ],
+      CURLOPT_POST => true,
+      CURLOPT_POSTFIELDS => http_build_query([
+        'message' => $message
+      ])
+    ];
+
+    $ch = curl_init($url);
+    curl_setopt_array($ch, $options);
+    $data = curl_exec($ch);
+    $err = curl_error($ch);
+    $errno = curl_errno($ch);
+    curl_close($ch);
+
+    return $err || $errno ? false : $data;
   }
 }
